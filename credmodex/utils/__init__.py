d1f3210cee1ce335ac0875c9f73abaf7bb637033
Gustavo Meira <gustavojvmf@gmail.com>
2025-06-05 11:35:47 -0300

    :recycle: <utils> plot migration analysis

--- credmodex/utils/__init__.py
+++ credmodex/utils/__init__.py
@@ -1,10 +1,13 @@
 from .design import (
     plotly_main_layout,
     plotly_main_subplot_layout,
-    matplotlib_main_layout
+    matplotlib_main_layout,
+)
+from .plots import (
+    plot_migration_analysis,
 )
 from .pdf_report import (
-    PDF_Report
+    PDF_Report,
 )
 
 
@@ -12,5 +15,8 @@ __all__ = [
     'plotly_main_layout',
     'plotly_main_subplot_layout',
     'matplotlib_main_layout',
-    'PDF_Report'
+    
+    'plot_migration_analysis',
+
+    'PDF_Report',
 ]
---
+++ credmodex/utils/plots.py
@@ -0,0 +1,149 @@
+import pandas as pd
+import numpy as np
+from typing import Literal
+
+import plotly.graph_objects as go
+
+from .design import plotly_main_layout
+
+
+
+
+
+def plot_migration_analysis(df:pd.DataFrame=None, index:str='rating', column:str='rating', target:str=None, 
+                            agg_func:str='count', z_normalizer:int=None, z_format:str=None, replace_0_None:bool=False, 
+                            time_col:str=None, initial_date:str=None, upto_date:str=None, sample:float=None, 
+                            width=800, height=600, show_fig:bool=True, colorscale:str='algae', xaxis_side:str='bottom', 
+                            split:list|Literal['train','test','oot']=['train','test','oot']):
+    '''
+    Analyzes migration patterns within a dataset by aggregating values based on the given parameters. 
+    The function generates a heatmap visualization of migration trends based on rating changes over time.
+    
+    ## Parameters
+
+    - ```index``` : str, default='rating'
+        Column name to be used as the index (rows) in the pivot table.
+    
+    - ```column``` : str, default='rating'
+        Column name to be used as the columns in the pivot table.
+    
+    - ```agg_func``` : str, default='count'
+        Aggregation function to be applied when summarizing data. Examples: 'sum', 'mean', 'count'.
+    
+    - ```z_normalizer``` : int, optional
+        A normalization factor to adjust the z-values in the heatmap.
+    
+    - ```initial_date``` : str, optional
+        The starting date (YYYY-MM-DD) for filtering the data.
+    
+    - ```upto_date``` : str, optional
+        The ending date (YYYY-MM-DD) for filtering the data.
+    
+    - ```show_fig``` : bool, default=True
+        If True, displays the generated heatmap.
+    
+    - ```colorscale``` : str, default='algae'
+        Color scheme for the heatmap. Recommended options: 'algae', 'dense', 'amp'.
+    
+    - ```xaxis_side``` : str, default='bottom'
+        Defines the placement of the x-axis labels ('top' or 'bottom').
+    
+    - ```z_format``` : str, optional
+        Format specifier for the z-values in the heatmap.
+    
+    ## Returns:
+
+    If `show_fig` is True:
+        - Displays a heatmap visualization.
+    If `show_fig` is False:
+        - Returns a pivot table summarizing migration trends.
+
+    '''
+    if (df is not None):
+        dff = df.copy(deep=True)
+    else:
+        raise ValueError(f"No dataframe defined!")
+
+    if (target is None):
+        raise ValueError(f"No target defined")
+
+    if isinstance(split, str):
+        split = [split]
+    try:
+        dff = dff[dff['split'].isin(split)]
+    except:
+        raise ValueError("Invalid split value. Use 'train', 'test' or 'oot'.")
+        
+    if (sample is not None):
+        sample = np.abs(sample)
+        if (sample <= 1):
+            dff = dff.sample(frac=sample)
+        elif (sample > 1):
+            if (sample > len(dff)):
+                raise ValueError(f"Sample size {sample} is larger than the dataset size {len(dff)}.")
+            dff = dff.sample(n=sample)
+    
+    if (initial_date is not None):
+        try:
+            initial_date = pd.to_datetime(initial_date)
+            dff = dff[dff[time_col] >= initial_date]
+        except Exception as e:
+            raise ValueError(f"Failed to parse initial_date or filter DataFrame: {e}")
+
+    if (upto_date is not None):
+        try:
+            upto_date = pd.to_datetime(upto_date)
+            dff = dff[dff[time_col] <= upto_date]
+        except Exception as e:
+            raise ValueError(f"Failed to parse initial_date or filter DataFrame: {e}")
+
+    if (column == index):
+        dff[f'{column}_'] = dff[column]
+        index = f'{column}_'
+
+    migration_dff = dff.groupby([index, column], observed=False)[target].agg(func=agg_func).reset_index().pivot(
+        columns=column, index=index, values=target
+    )
+    if replace_0_None:
+        migration_dff = migration_dff.replace(0, np.nan)
+
+    if z_normalizer is None:
+        z = list(reversed(migration_dff.values.tolist()))
+        texttemplate = "%{z:.2f}"
+    elif z_normalizer == 0:
+        migration_dff = migration_dff.div(migration_dff.sum(axis=1), axis=0)
+        z = list(reversed(migration_dff.values.tolist()))
+        texttemplate = "%{z:.2f}"
+    elif z_normalizer == 1:
+        migration_dff = migration_dff.div(migration_dff.sum(axis=0), axis=1)        
+        z = list(reversed(migration_dff.values.tolist()))
+        texttemplate = "%{z:.2f}"
+
+    if z_format == 'percent':
+        z = [[elem * 100 for elem in sublist] for sublist in z]
+        migration_dff = (100*migration_dff.round(4)).fillna('-')
+        texttemplate = "%{z:.2f}"
+    elif z_format == 'int':
+        migration_dff = migration_dff.fillna(-10000).astype(int).replace(-10000,'-')
+        texttemplate = "%{z:.0f}"
+    else:
+        try: migration_dff = migration_dff.round(3).fillna('-')
+        except: migration_dff = migration_dff.fillna(-10000).astype(int).replace(-10000,'-')
+
+    fig = go.Figure()
+    fig.add_trace(go.Heatmap(
+            z=z,
+            x=migration_dff.columns,
+            y=list(reversed(migration_dff.index)),
+            hoverongaps=False,
+            colorscale=colorscale,
+            texttemplate=texttemplate
+    ))
+    plotly_main_layout(
+        fig, title='Migration', x=column, y=index, width=width, height=height,
+    )
+    fig.update_layout({'xaxis':dict(gridcolor='#EEE',side=xaxis_side), 'yaxis':dict(gridcolor='#EEE')})
+    
+    if show_fig: return fig
+    else: return migration_dff
+
